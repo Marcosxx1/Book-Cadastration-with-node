869d37970dec43e096f02184e4cb6ee4c0a16f64
Marcos Alexandre da Silva <marcos.s@compasso.com.br>
2023-07-07 14:30:37 -0400

    crud inside the app finishied

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-import express from 'express';
+import express, { json } from 'express';
+import uuidv4 from 'uuidv4'
 
 const app = express();
 
 app.use(express.json());
+
 const livros = [
-    { id: 1, nome: 'Algoritmos para viver', autor: 'Brian', categoria: 'Tecnologia', paginas: 500 }
-    , { id: 2, nome: 'Aprenda Node', autor: 'Brian', categoria: 'Tecnologia', paginas: 500 }
+    { id: 1, "titulo": "Livro 1" },
+    { id: 2, "titulo": "Livro 2" }
 ]
 
 app.get('/', (req, res) => {
@@ -16,4 +18,34 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 });
 
+app.get('/livros/:id', (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.json(livros[index]);
+})
+
+
+app.post('/livros', (req, res) => {
+    const livro = req.body;
+    livros.push(livro);
+    res.status(201).json("Livro cadastrada");
+})
+
+app.put('/livros/:id', (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index] = req.body;
+    console.log(livros);
+    res.status(200).json("Livro atualizada");
+});
+
+
+app.delete('/livros/:id', (req, res) => {
+    const { id } = req.params;
+    const index = buscaLivro(id);
+    livros.splice(index, 1);
+    res.send("Livro deletado");
+})
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => livro.id == id);
+}
 export default app;
