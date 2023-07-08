5c0879e7df1d9c29500d236246b3cda1d622c310
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-07-08 15:02:16 -0400

    Refactoring for MVC, LivroServices deals with business logic

---
+++ src/services/LivroServices.js
@@ -0,0 +1,37 @@
+import LivroRepository from "../repositories/LivroRepository.js";
+
+export class LivroService {
+	static getAllLivros = async () => {
+		return LivroRepository.getAllLivros();
+	};
+
+	static getLivroById = async id => {
+		const livro = await LivroRepository.getLivroById(id);
+		if (!livro) {
+			throw new Error("Livro not found");
+		}
+		return livro;
+	};
+
+	static createLivro = async livroData => {
+		return LivroRepository.createLivro(livroData);
+	};
+
+	static deleteLivro = async id => {
+		const livroRemovido = await LivroRepository.deleteLivro(id);
+		if (!livroRemovido) {
+			throw new Error("Livro not found");
+		}
+		return livroRemovido;
+	};
+
+	static updateLivro = async (id, livroData) => {
+		const livroAtualizado = await LivroRepository.updateLivro(id, livroData);
+		if (!livroAtualizado) {
+			throw new Error("Livro not found");
+		}
+		return livroAtualizado;
+	};
+}
+
+export default LivroService;
