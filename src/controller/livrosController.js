b0adc791f3eaf2f4db4da665f2cda142c1b97875
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-07-08 15:00:30 -0400

    Refactoring for MVC, livroController deals with errors

--- src/controller/livrosController.js
+++ src/controller/livrosController.js
@@ -1,65 +1,53 @@
-import livros from "../models/Livro.js";
+import LivroService from "../services/LivroServices.js";
 
 export class LivroController {
+	static listarLivros = async (req, res) => {
+		try {
+			const livrosEncontrados = await LivroService.getAllLivros();
+			res.status(200).json(livrosEncontrados);
+		} catch (error) {
+			res.status(500).json({ error: error.message });
+		}
+	};
 
-    static listarLivros = async (req, res) => {
-        try {
-            const livrosEncontrados = await livros.find();
-            res.status(200).json(livrosEncontrados);
-        }
-        catch (error) {
-            res.status(500).json({ error: error.message })
-        }
-    }
+	static buscarLivroPorId = async (req, res) => {
+		try {
+			const livroEncontrado = await LivroService.getLivroById(req.params.id);
+			res.status(200).json(livroEncontrado);
+		} catch (error) {
+			res.status(404).json({ message: error.message });
+		}
+	};
 
-    static buscarLivroPorId = async (req, res) => {
-        try { 
-            const livroEncontrado = await livros.findById(req.params.id);
-            res.status(200).json(livroEncontrado);
-        }
-        catch (error) {
-            res.status(500).json({ error: error.message })
-        }
-    }
+	static cadastrarLivro = async (req, res) => {
+		try {
+			const novoLivro = await LivroService.createLivro(req.body);
+			res.status(201).json(novoLivro);
+		} catch (error) {
+			res.status(500).json({ error: error.message });
+		}
+	};
 
-    static cadastrarLivro = async (req, res) => {
-        try {
-            const novoLivro = new livros(req.body);
-            const livroSalvo = await novoLivro.save();
-            res.status(201).json(livroSalvo);
-        }
-        catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    }
+	static apagarLivro = async (req, res) => {
+		try {
+			await LivroService.deleteLivro(req.params.id);
+			res.status(200).json({ message: "Livro removido com sucesso" });
+		} catch (error) {
+			res.status(404).json({ message: error.message });
+		}
+	};
 
-    static apagarLivro = async (req, res) => {
-        try {
-            const livroRemovido = await livros.findByIdAndDelete(req.params.id);
-            if (livroRemovido) {
-                res.status(200).json({ message: "Livro removido com sucesso" });
-            }
-            else {
-                res.status(404).json({ message: "Livro não encontrado" });
-            }
-        }
-        catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    }
-
-    static atualizarLivro = async (req, res) => {
-        try {
-            const livroAtualizado = await livros.findByIdAndUpdate(req.params.id,
-                { $set: req.body },
-                { new: true });
-            res.status(200).json(livroAtualizado);
-        }
-        catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    }
+	static atualizarLivro = async (req, res) => {
+		try {
+			const livroAtualizado = await LivroService.updateLivro(
+				req.params.id,
+				req.body
+			);
+			res.status(200).json(livroAtualizado);
+		} catch (error) {
+			res.status(404).json({ message: error.message });
+		}
+	};
 }
 
-
-export default LivroController; 
+export default LivroController;
